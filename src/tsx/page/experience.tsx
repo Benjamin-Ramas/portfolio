7fabebda7c791830202d3cd87b873e90bd85f731
Benjamin Ramas <benjaminramas119@gmail.com>
2024-07-03 17:45:14 +0100

    Updated Experience Section

--- src/tsx/page/experience.tsx
+++ src/tsx/page/experience.tsx
@@ -1,18 +1,27 @@
 function Experience () {
     return(
         <>
-            <section className="previous-work">
-                <hr /><h1>Previous Work</h1><hr />
-                <h2>Data Analyst at RoslinCT</h2>
-                <h3>February 2024 - Present</h3>
-                <p></p>
+            <section className="experience-section previous-work">
+                <h1 className="page-title">Previous Experience</h1>
+                <h1>Previous Work</h1>
+                        <div className="experience-details">
+                        <h2>Data Analyst at RoslinCT</h2>
+                        <h3>February 2024 - Present</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea, nam facilis eaque fugit officia, aperiam impedit, at tempore deleniti voluptate vero doloremque provident debitis nisi! Itaque iste ratione atque!</p>
+                    </div>
             </section>
-            <section className="education">
+            <section className="experience-section education">
                 <h1>Education</h1>
-                <h2>NQ Computing at West Lothian College</h2>
-                <h3>September 2023 - June 2023</h3>
-                <h2>Senior Highschool at University of San Carlos</h2>
-                <h3>August 2019 - May 2022</h3>
+                    <div className="experience-details">
+                        <h2>NQ Computing at West Lothian College</h2>
+                        <h3>September 2023 - June 2023</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti repudiandae illum facere quas delectus tempore, a quo dignissimos in incidunt voluptatum placeat dolor voluptas, illo ea laudantium nulla quibusdam at.</p>
+                    </div>
+                    <div className="experience-details">
+                        <h2>Senior Highschool at University of San Carlos</h2>
+                        <h3>August 2019 - May 2022</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti repudiandae illum facere quas delectus tempore, a quo dignissimos in incidunt voluptatum placeat dolor voluptas, illo ea laudantium nulla quibusdam at.</p>
+                    </div>
             </section>
         </>
     )
