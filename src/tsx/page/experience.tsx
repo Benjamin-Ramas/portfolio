f190983c66d026ddf185aed5115a76dbecbdd6f4
Benjamin Ramas <benjaminramas119@gmail.com>
2024-07-16 18:02:23 +0100

    Changed experience descriptions

--- src/tsx/page/experience.tsx
+++ src/tsx/page/experience.tsx
@@ -8,7 +8,7 @@ function Experience () {
                     <div className="experience-details">
                         <h2>Data Analyst at RoslinCT</h2>
                         <h3>February 2024 - Present</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea, nam facilis eaque fugit officia, aperiam impedit, at tempore deleniti voluptate vero doloremque provident debitis nisi! Itaque iste ratione atque!</p>
+                        <p>I am currently working at RoslinCT as a Data Analyst providing my knowledge and skills in assisting with developing systems in which data could be used more efficiently.</p>
                     </div>
                 </a>
             </section>
@@ -18,14 +18,23 @@ function Experience () {
                     <div className="experience-details">
                         <h2>NQ Computing at West Lothian College</h2>
                         <h3>September 2023 - June 2024</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti repudiandae illum facere quas delectus tempore, a quo dignissimos in incidunt voluptatum placeat dolor voluptas, illo ea laudantium nulla quibusdam at.</p>
+                        <p>I studied NQ Computing at the West Lothian College so as to learn more and better my existing skills that I learnt on my own to more efficiently utilize them.</p>
                     </div>
                 </a>
                 <a className="linkless" href="https://usc.edu.ph/" target="blank">
                     <div className="experience-details">
                         <h2>Senior Highschool at University of San Carlos</h2>
                         <h3>August 2019 - May 2022</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti repudiandae illum facere quas delectus tempore, a quo dignissimos in incidunt voluptatum placeat dolor voluptas, illo ea laudantium nulla quibusdam at.</p>
+                        <p>I studied under the University of San Carlos under the STEM course during my senior highschool years to get a better grasp of the science, technology, engineering, and math that I had developed a great interest in during my youth.</p>
+                    </div>
+                </a>
+            </section>
+            <section className="experience-section personal-projects">
+                <h1>Personal Projects</h1>
+                <a className="linkless" href="https://blankedslate.itch.io/daggers-and-time-loops" target="blank">
+                    <div className="experience-details">
+                        <h2>Daggers and Timeloops</h2>
+                        <p>A personal project that I developed in my free time for the GMTK game jam during 2023 where out of 6,752 entries I scored in the top #74 in creativity. With an overall review score of 3.8/5</p>
                     </div>
                 </a>
             </section>
